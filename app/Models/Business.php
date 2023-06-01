6b9b6fe036f964c6db91c64b436f19106b73bf56
amine.lamchatab <115371227+LamchatabAmine@users.noreply.github.com>
2023-06-01 21:52:01 +0100

    complete db

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -11,6 +11,23 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Business extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'city_id',
+        'title',
+        'logo',
+        'description',
+        'website',
+        'address',
+        'created_at',
+        'updated_at',
+    ];
+
+
+
+
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
